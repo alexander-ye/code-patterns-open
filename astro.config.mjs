0d9bb3f91308cefee0cd597c205a999ad1e33e75
alexander-ye <alexander.ye7@gmail.com>
2023-07-11 18:49:56 -0400

    removed broken import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import remarkToc from "remark-toc";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    mdx({
-      remarkPlugins: [remarkToc],
-    }),
-    react(),
-  ],
+  integrations: [mdx(), react()],
   markdown: {
     // Applied to .md and .mdx files
-    remarkPlugins: [remarkToc],
   },
 });
