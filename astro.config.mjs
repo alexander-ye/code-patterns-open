c956aca6aa12be90a3a3f45817b77c22beac37b9
alexander-ye <alexander.ye7@gmail.com>
2023-07-11 18:48:45 -0400

    fixed typos, added table of contents

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import remarkToc from "remark-toc";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()]
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkToc],
+    }),
+    react(),
+  ],
+  markdown: {
+    // Applied to .md and .mdx files
+    remarkPlugins: [remarkToc],
+  },
 });
